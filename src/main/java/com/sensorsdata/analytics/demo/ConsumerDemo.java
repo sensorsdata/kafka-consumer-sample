5504be8f1ec3a02978b407e182a380cc0ea8f4f3
Codievilky August <zhangaoge@sensorsdata.cn>
2020-10-28 12:00:28 +0800

    增加了日志依赖与一些注释

--- src/main/java/com/sensorsdata/analytics/demo/ConsumerDemo.java
+++ src/main/java/com/sensorsdata/analytics/demo/ConsumerDemo.java
@@ -13,9 +13,11 @@ import java.util.List;
  */
 public class ConsumerDemo {
   public static void main(String[] args) {
-    String[] brokerList = new String[] {"10.19.91.39:9092"};// 为内网环境的 kafka broker 的地址
+    // 为内网环境的 kafka broker 的地址
+    // 请注意！！！ kafka 最终会使用机器的 hostname 进行连接，因此需要将 hostname 配置到当前机器的 /etc/hosts 中
+    String[] brokerList = new String[] {"10.19.91.39:9092"};
     SensorsSimpleConsumer sensorsSimpleConsumer = new SensorsSimpleConsumer(brokerList);
-    sensorsSimpleConsumer.assignByTopic(Collections.singleton("master-test")); // 指定订阅的 topic 名
+    sensorsSimpleConsumer.assignByTopic(Collections.singleton("event_topic")); // 指定订阅的 topic 名
     List<String> records;
     while (true) {
       records = sensorsSimpleConsumer.pollRecords(1000); // 订阅数据
